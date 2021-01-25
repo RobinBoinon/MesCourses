fd77ffe17b507e90a2ceeaaadffbf20ed8d653ab
Robin Boinon <boinon.robin@gmail.com>
2021-01-25 09:42:10 +0100

    ajout entities

---
+++ src/Entity/Produit.php
@@ -0,0 +1,58 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\ProduitRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity(repositoryClass=ProduitRepository::class)
+ */
+class Produit
+{
+    /**
+     * @ORM\Id
+     * @ORM\GeneratedValue
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $name;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $image_link;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getName(): ?string
+    {
+        return $this->name;
+    }
+
+    public function setName(string $name): self
+    {
+        $this->name = $name;
+
+        return $this;
+    }
+
+    public function getImageLink(): ?string
+    {
+        return $this->image_link;
+    }
+
+    public function setImageLink(string $image_link): self
+    {
+        $this->image_link = $image_link;
+
+        return $this;
+    }
+}
